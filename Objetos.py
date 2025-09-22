af539a7e6971bad1fc5f5c26a112dd090e6776da
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-09-22 14:45:39 +0200

    avanzando en clases en pyton

---
+++ Objetos.py
@@ -0,0 +1,28 @@
+class Persoa:
+
+    def __init__(self, nome, dni, edade):
+        self.nome=nome
+        self.dni = dni
+        self.edade = edade
+
+    def comprobarEdade(self,id):
+        if id >=0 and id < 100:
+            self.edade = id
+        else:
+            self.edade = 0
+
+
+p = Persoa("Manuel", "356L",34)
+
+print (p.nome)
+
+class Traballador (Persoa,Posto):
+    def __init__(self, nome,dni,idade,NUSS, salario, formacion):
+        super().__init__(nome,dni,idade)
+        self.NUSS = NUSS
+        self.salario = salario
+        self.formacion = formacion
+
+class Posto ():
+    def __init__(self, tarefa,horario,remuneracion,formacion):
+
