e119e7087d9d439f3d14d465d42ad1ef37a2042f
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-09-25 10:18:25 +0200

    avanzando en listas

--- coleccions.py
+++ coleccions.py
@@ -38,3 +38,25 @@ d = { 1 : "Un",
 }
 
 # len(coleccion) -> devuleve la longitud de la lista.
+
+# Esta es otra manera de de crear una lista
+l3 = list((2,3,4,5,6))
+
+t3 = tuple((2,3,4,5,6))
+
+d3 = dict( ( (1,"un"), (2,"dous"), (3,"tres") ) )
+
+l2.append([3,4,5]) # Añade un elemento al final de la lista
+l2.extend([6,7,8]) # Añade varios elementos al final de la lista
+l2.insert(2,"un elemento") # Inserta un elemento en la posicion 2
+l2.count(2) # Cuenta las veces que aparece el elemento 2 en la lista
+l2.remove("un elemento") # Elimina el elemento indicado
+l2.__len__() # Devuelve la longitud de la lista
+l2.index(3,3,7) # Devuelve la posicion del elemento 3 entre las posiciones 3 y 7
+l2.pop() # Elimina y devuelve el ultimo elemento de la lista
+l2.pop(3) # Elimina y devuelve el elemento en la posicion 3
+l2.remove(3) # Elimina la primera ocurrencia del elemento 3
+l2.reverse() # Invierte la lista
+l3=l2[::-1] # Otra manera de invertir la lista pero creando una nueva y la original no se modifica
+l2.sort() # Ordena la lista
+
